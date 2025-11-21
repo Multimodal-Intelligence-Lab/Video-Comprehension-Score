67d0b9bd0ff8897c8d0d44591664e0ba228b970c
hdubey-debug <62215781+hdubey-debug@users.noreply.github.com>
2025-11-20 19:57:49 -0600

    refactor: Rename `_mapping_windows` to `_alignment_windows` and `_las` to `_local_sas` imports.

--- src/vcs/_visualize_vcs/__init__.py
+++ src/vcs/_visualize_vcs/__init__.py
@@ -1,15 +1,15 @@
 from ._config import visualize_config
 from ._text_chunks import visualize_text_chunks
 from ._similarity_matrix import visualize_similarity_matrix
-from ._mapping_windows import visualize_mapping_windows
+from ._alignment_windows import visualize_mapping_windows
 from ._best_match import visualize_best_match
 from ._local_nas import (
     visualize_local_nas,
     visualize_local_nas_precision_calculations,
     visualize_local_nas_recall_calculations,
-)  
+)
 from ._global_nas import visualize_global_nas
-from ._las import visualize_las, visualize_las_load_sharing
+from ._local_sas import visualize_las, visualize_las_load_sharing
 from ._metrics_summary import visualize_metrics_summary
 from ._pdf_report import create_vcs_pdf_report
 
